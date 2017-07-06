374480074832df5ca97a664c51b610a6cb355565
cesar-neto <cesar.neto@ccc.ufcg.edu.br>
2017-07-06 11:58:21 -0300

    rotas ativas lado servidor e interno

--- apiweb/src/main/resources/static/js/app.js
+++ apiweb/src/main/resources/static/js/app.js
@@ -1,15 +1,17 @@
 //criacao do modulo principal da aplicacao
 var appCliente = angular.module("appCliente", ['ngRoute']);
 
-appCliente.config(function($routeProvider){
+appCliente.config(function($routeProvider, $locationProvider){
 	
-	$routeProvider.when("/clientes", {
-		templateUrl: 'view/cliente.html',
-		controller: 'clienteController'
-			
-	}).otherwise({
-		rediretTo:'/'
+	$routeProvider
+	.when("/clientes", { templateUrl: 'view/cliente.html', controller: 'clienteController'})
+	.when("/cidades", { templateUrl: 'view/cidade.html', controller: 'cidadeController'})
+	.when("/estados", { templateUrl: 'view/estado.html', controller: 'estadoController'})
+	.otherwise({redirectTo: '/'});
+	
+	$locationProvider.html5Mode({
+		  enabled: true,
+		  requireBase: false
 	});
-	
 });
 
