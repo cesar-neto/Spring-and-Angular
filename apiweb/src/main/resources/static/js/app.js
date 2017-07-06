8756675886251ff2e8cd4d9407f568fe8e6fa1f0
cesar-neto <cesar.neto@ccc.ufcg.edu.br>
2017-07-06 10:09:09 -0300

    Separando controllers e criando rotas

---
+++ apiweb/src/main/resources/static/js/app.js
@@ -0,0 +1,15 @@
+//criacao do modulo principal da aplicacao
+var appCliente = angular.module("appCliente", ['ngRoute']);
+
+appCliente.config(function($routeProvider){
+	
+	$routeProvider.when("/clientes", {
+		templateUrl: 'view/cliente.html',
+		controller: 'clienteController'
+			
+	}).otherwise({
+		rediretTo:'/'
+	});
+	
+});
+
---
+++ apiweb/src/main/resources/static/js/controller/cliente-controller.js
@@ -0,0 +1,64 @@
+//criacao de controllers
+appCliente.controller("clienteController", function($scope, $http){
+
+	$scope.clientes = [];
+	$scope.cliente = {};
+	
+	$scope.carregarClientes = function(){
+		$http({
+		  method: 'GET',
+		  url: 'http://localhost:8080/clientes/'
+		}).then(function successCallback(response) {
+		    $scope.clientes=response.data;
+		    console.log(response.data);
+		    console.log(response.status);
+		  }, function errorCallback(response) {
+			  console.log(response.data);
+			  console.log(response.status);
+		  });
+	};
+	
+	$scope.carregarClientes();
+		
+	$scope.salvarCliente = function(){
+		$http({
+		  method: 'POST',
+		  url: 'http://localhost:8080/clientes/',
+		  data: $scope.cliente
+		}).then(function successCallback(response) {
+		    //$scope.clientes.push(response.data);
+			$scope.carregarClientes();
+			$scope.cliente={};
+		    console.log(response.data);
+		    console.log(response.status);
+		  }, function errorCallback(response) {
+			  console.log(response.data);
+			  console.log(response.status);
+		  });
+	};
+	
+	$scope.excluirCliente = function(cliente){
+		$http({
+		  method: 'DELETE',
+		  url: 'http://localhost:8080/clientes/'+cliente.id
+		}).then(function successCallback(response) {
+		    $scope.clientes.splice(cliente.id-1,1);
+		    console.log(response.data);
+		    console.log(response.status);
+		  }, function errorCallback(response) {
+			  console.log(response.data);
+			  console.log(response.status);
+		  });
+	};
+	
+	$scope.cancelarEdicao = function(){
+		$scope.cliente = {};		
+		
+	}
+	
+	$scope.editarCliente = function(cli){
+		$scope.cliente = angular.copy(cli);
+		
+	};
+	
+});
