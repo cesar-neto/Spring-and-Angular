1e42458362cec5e93ac31f3934f73a31dec06d7f
cesar-neto <cesar.neto@ccc.ufcg.edu.br>
2017-07-26 15:34:02 -0300

    auto-complet and rest

--- apiweb/src/main/resources/static/js/controller/cliente-detalhe-controller.js
+++ apiweb/src/main/resources/static/js/controller/cliente-detalhe-controller.js
@@ -2,22 +2,16 @@ appCliente.controller("clienteDetalheController", function($scope, $routeParams,
 
 	$scope.cliente = {};
 	
-	$scope.tags = [
-	               { text: 'Html' },
-	               { text: 'Java' },
-	               { text: 'C#' },
-	               { text: 'Django' },
-	               { text: 'Angular' },
-	               { text: 'Swagger' },
-	               { text: 'JavaScript' },
-	               { text: 'Python' },
-	               { text: 'React' },
-	               { text: 'Css' }
-	             ];
-	
-	$scope.loadTags = function(query) {
-        return $http.get('/tags?query=' + query);
-   };
+	$scope.tags = [];
+	  
+	  $scope.loadCountries = function($query) {
+	    return $http.get('json/countries.json', { cache: true}).then(function(response) {
+	      var countries = response.data;
+	      return countries.filter(function(country) {
+	        return country.name.toLowerCase().indexOf($query.toLowerCase()) != -1;
+	      });
+	    });
+	  };
 	
 	$http.get("admin/clientes/"+$routeParams.clienteId).then(function(response){
 		
